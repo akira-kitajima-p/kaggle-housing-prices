80d2d8207ffb8a387bb17f385b64a366e5693260
akira-kitajima-p <progressive@live.jp>
2025-02-24 02:51:44 +0900

    Update house-price.py

--- solve/house-price.py
+++ solve/house-price.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from pretreatment import fill_nan, str2code
+from pretreatment import fill_nan, firstIdea, str2code
 from pretreatment import drop_str
 
 train = pd.read_csv('../datas/train.csv')
@@ -12,13 +13,10 @@ pred = pd.read_csv('../datas/sample_submission.csv')
 data = pd.concat([train, test], sort=False)
 
 data = str2code(data)
-
-print(data.head())
 data = drop_str(data)
 data = fill_nan(data)
-print(data.head())
-print(data.isnull().sum().to_string())
 
+data = firstIdea(data)
 
 # 訓練データとテストデータを前処理を行ったデータに置き換える
 train = data[:len(train)]
@@ -30,10 +28,11 @@ X_train = train.drop('SalePrice', axis=1)
 X_test = test.drop('SalePrice', axis=1)
 
 # 学習
-clf = LogisticRegression(penalty='l2', solver="sag", random_state=0, max_iter=1000)
-print("LogsticRegression()")
+# clf = LogisticRegression(penalty='l2', solver="sag", random_state=0, max_iter=5000)
+clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
+print("set clf")
 clf.fit(X_train, y_train)
 print("fit()")
 pred['SalePrice'] = clf.predict(X_test)
 
-pred.to_csv('sample_out', index=False)
+pred.to_csv('sample_out.csv', index=False)
